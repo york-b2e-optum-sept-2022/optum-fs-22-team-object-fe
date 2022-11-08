0aaf27efc98fda5b5ef69edebd3acebcdb6edb19
Dano McKenzie <danomckenzie@gmail.com>
2022-11-08 14:12:19 -0700

    create admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {CreateAdminComponent} from "./create-admin/create-admin.component";
+import {LoginComponent} from "./login/login.component";
 
 
 const routes: Routes = [
-  {path: 'administrator', component: CreateAdminComponent}
+  {path: '', redirectTo:'administrator', pathMatch:'full'},
+  {path: 'administrator', component: CreateAdminComponent},
+  {path: 'login', component: LoginComponent}
 ]
 
 @NgModule({
