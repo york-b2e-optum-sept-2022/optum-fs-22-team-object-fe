5677bfde166cbc15039f527127a1ebbc9f841f76
Dano McKenzie <danomckenzie@gmail.com>
2022-11-09 08:18:56 -0700

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {CreateAdminComponent} from "./create-admin/create-admin.component";
 import {LoginComponent} from "./login/login.component";
+import {MainAdminComponent} from "./main-admin/main-admin.component";
+import {MainShopKeeperComponent} from "./main-shop-keeper/main-shop-keeper.component";
 
 
 const routes: Routes = [
   {path: '', redirectTo:'administrator', pathMatch:'full'},
   {path: 'administrator', component: CreateAdminComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'main-admin', component: MainAdminComponent},
+  {path: 'main-shopkeepeer', component: MainShopKeeperComponent}
 ]
 
 @NgModule({
